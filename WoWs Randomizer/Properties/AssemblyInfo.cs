a4304b35cd5d42b0aa90cf3edfa652a594afb9b5
Kenneth Axi <kenneth.axi@outlook.com>
2020-09-09 22:38:35 +0200

    New version: 1.6.8.17

--- WoWs Randomizer/Properties/AssemblyInfo.cs
+++ WoWs Randomizer/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 // Version currently in production: 1.6.7.16
 // Version under development: 1.6.8.17
-[assembly: AssemblyVersion("1.6.7.16")]
-[assembly: AssemblyFileVersion("1.6.7.16")]
+[assembly: AssemblyVersion("1.6.8.17")]
+[assembly: AssemblyFileVersion("1.6.8.17")]

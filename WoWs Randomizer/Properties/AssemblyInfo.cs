2d0f7781ada1ab06b4f80d272407905b47c35bd1
Kenneth Axi <kenneth.axi@outlook.com>
2020-11-09 20:25:38 +0100

    New version

--- WoWs Randomizer/Properties/AssemblyInfo.cs
+++ WoWs Randomizer/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-// Version currently in production: 1.7.3.24
-// Version under development: 1.7.3.25
-[assembly: AssemblyVersion("1.7.3.24")]
-[assembly: AssemblyFileVersion("1.7.3.24")]
+// Version currently in production: 1.7.4.25
+// Version under development: 1.7.6.26
+[assembly: AssemblyVersion("1.7.4.25")]
+[assembly: AssemblyFileVersion("1.7.4.25")]
